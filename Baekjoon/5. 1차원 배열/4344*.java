0c037ec4a250d5de873a8351f9f55a78fffd544d
SoYeonKim522 <87492707+SoYeonKim522@users.noreply.github.com>
2021-09-28 23:23:15 +0900

    Update 4344*.java
    틀린 이유 찾아서 수정

--- Baekjoon/5. 1차원 배열/4344*.java
+++ Baekjoon/5. 1차원 배열/4344*.java
@@ -10,11 +10,14 @@ public class Main {
         
         int C = Integer.parseInt(br.readLine());
         int[] arr;
-        int sum = 0;
+//         int sum = 0;
         int avg = 0;
-        int count = 0;
+//         int count = 0;
         
             for(int i = 0; i < C; i++){
+                int sum = 0;
+                int count = 0;         //⭑⭑⭑ for 문 안에서 초기화 필요 ⭑⭑⭑
+                
                 st = new StringTokenizer(br.readLine()," ");
                 int n = Integer.parseInt(st.nextToken());
                 arr = new int[n];
@@ -30,8 +33,9 @@ public class Main {
                         count ++;
                     }
                 }
-                double percent = (double) count/n * 100;        //double 로 캐스팅!!!
+                double percent = (double) count/n * 100;        //double 로 캐스팅!!! 또는 위에서 count 를 double/float 로 선언
                 System.out.println(String.format("%.3f", percent)+"%");
+                //ㄴ 다른 방법 : System.out.printf("%.3f%% \n",(count/(double)n) * 100);
             }
             
         }
